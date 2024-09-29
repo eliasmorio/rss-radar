96b44cdf65b65ff7d5a46fa1e4fafc7841f63953
eliasmorio <83926043+eliasmorio@users.noreply.github.com>
2024-09-29 13:08:51 +0200

    fix: config and scheduling

---
+++ services/web-api/src/main/java/fr/emorio/service/SchedulerService.java
@@ -0,0 +1,24 @@
+package fr.emorio.service;
+
+import fr.emorio.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SchedulerService {
+
+    private final FeedRepository feedRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void schedule() {
+        feedRepository.findAll().forEach(feed -> {
+            rabbitTemplate.convertAndSend("", feed.getUrl());
+        });
+    }
+
+
+}
